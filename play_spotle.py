23483c1b68364bdcd5852c989efb22fec8ff56a3
Tommy Rennolds <thomasrennolds@gmail.com>
2022-04-13 18:29:01 -0400

    Created classes

---
+++ play_spotle.py
@@ -0,0 +1,12 @@
+from spotle import Spotle
+
+def main():
+    print("Welcome to Spotle!")
+    spotle = Spotle()
+    print(spotle)
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ spotle.py
@@ -0,0 +1,4 @@
+class Spotle:
+
+    def __init__(self):
+        pass
