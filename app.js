6a4a37b332267693db4d288d3d5bc9c8cce7a7c0
rennolds <46907691+rennolds@users.noreply.github.com>
2022-04-16 19:51:47 -0400

    can guess artists again, guess count increments, checks for previously guessed artists

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let searchable = [
     'Taylor Swift',
     'The Weeknd',
     'Dua Lipa',
+    'Drake',
   ];
   
   const searchInput = document.getElementById('search');
@@ -12,7 +13,13 @@ let searchable = [
   const resultsWrapper = document.querySelector('.results');
   const guessContainer = document.querySelector('.guess-container');
   const guessButton = document.querySelector('.guess-btn');
+  const intro = document.getElementById('intro');
+  const guessCountContainer = document.querySelector('.guesses');
+  let firstGuess = true;
+  let guessCount = 1;
+  let guessedArtists = [];
   
+  //handles autocomplete 
   searchInput.addEventListener('keyup', () => {
     let results = [];
     let input = searchInput.value;
@@ -39,9 +46,11 @@ let searchable = [
     resultsWrapper.innerHTML = `<ul>${content}</ul>`;
   }
 
+  //when user selects an artist from search results -> put the artist in search bar and hide other results
   resultsWrapper.addEventListener("click", function() {
       var clickedElement = document.querySelector('li:hover');
       searchInput.value = clickedElement.innerHTML;
+      searchWrapper.classList.remove('show');
   }); 
 
   class Artist {
@@ -57,30 +66,51 @@ let searchable = [
 
 
 var artists = new Map();
-artists.set('the weeknd', new Artist("The Weeknd",1,2013,"Canadian","Pop","Male"))
-artists.set('justin bieber', new Artist("Justin Bieber",2,2010,"Canadian","Pop","Male"))
-artists.set('ed sheeran', new Artist("Ed Sheeran",3,2011,"English","Pop","Male"))
-artists.set('dua lipa', new Artist("Dua Lipa",4,2017,"English","Pop","Female"))
-artists.set('taylor swift', new Artist("Taylor Swift",5,2006,"American","Pop","Female"))
-artists.set('coldplay', new Artist("Coldplay",6,2000,"American","Alternative","Male"))
+artists.set('the weeknd', new Artist("The Weeknd",1,2013,"Canada","Pop","Male"));
+artists.set('justin bieber', new Artist("Justin Bieber",2,2010,"Canada","Pop","Male"));
+artists.set('ed sheeran', new Artist("Ed Sheeran",3,2011,"England","Pop","Male"));
+artists.set('dua lipa', new Artist("Dua Lipa",4,2017,"England","Pop","Female"));
+artists.set('taylor swift', new Artist("Taylor Swift",5,2006,"United States","Pop","Female"));
+artists.set('coldplay', new Artist("Coldplay",6,2000,"United States","Alternative","Male"));
+artists.set('drake', new Artist("Drake", 7, 2006, "Canada", "Rap",  "Male"));
 const mysteryArtist = artists.get("justin bieber")
 
+//handle guess when player searches for an artist
 const handleGuess = () => {
     let guess = searchInput.value
-    guess = guess.toLowerCase()
-    if (artists.get(guess) != null) {
-        var currentArtist = artists.get(guess)
-        if (currentArtist.name == mysteryArtist.name) {
-            win()
-        }
-        else {
-            incorrectGuess(currentArtist)
-        }
+    searchInput.value = ""; //make search bar empty 
+    searchWrapper.classList.remove('show'); //hide results
+    guess = guess.toLowerCase() //make guess lowercase
 
+    if (guess == "") { //empty guess, do nothing
+      return;
     }
-    else {
-        invalidArtist()
+    if (artists.get(guess) == null) { //invalid artist, not in top 500
+      invalidArtist();
+      return;
     }
+    
+    var currentArtist = artists.get(guess);
+    if (currentArtist.name == mysteryArtist.name) {
+      if (firstGuess) {
+        firstGuess = false;
+        intro.parentNode.removeChild(intro);
+      }
+      win();
+      return;
+    }
+
+    if (guessedArtists.includes(currentArtist.name)) {
+      console.log('already guessed');
+      return;
+    }
+    
+    if (firstGuess) {
+      firstGuess = false;
+      intro.parentNode.removeChild(intro);
+    }
+    
+    incorrectGuess(currentArtist);
 }
 
 function invalidArtist() {
@@ -93,21 +123,107 @@ function win() {
     return;
 }
 
-function incorrectGuess(currentArtist) {
-    
-    const guessElement = document.createElement('div');
-    guessElement.classList.add('guess');
-    
-    const row1 = document.createElement('div');
-    row1.classList.add('guessRow');
+function incorrectGuess(guess) {
 
+    printGuess(guess);
 
-    guessElement.append('row1');
-    guessElement.append('row2');
-    guessElement.append('row3');
-    guessContainer.prepend(guessElement);
+    guessCount++;
+    guessCountContainer.innerHTML = "Guess " + guessCount + " of 6";
+    if (guessCount == 7) {
+      console.log("lost");
+    }
+
+    guessedArtists.push(guess.name);
 
     return;
+}
+
+function printGuess(guess) {
+  const guessElement = document.createElement('div');
+  guessElement.classList.add('guess');
+
+  //create row1
+  const row1 = document.createElement('div');
+  row1.classList.add('guessRow');
+
+  //create name div and image div
+  const nameElement = document.createElement('div');
+  nameElement.classList.add('item-name');
+  nameElement.innerHTML = guess.name;
+  const imageArtist = document.createElement('img');
+  imageArtist.src = "resources\\artists\\" + guess.name + ".jpg";
+  
+  //add image to nameElement div
+  nameElement.prepend(imageArtist);
+  row1.append(nameElement);
+
+  //row2
+  const row2 = document.createElement('div');
+  row2.classList.add('guessRow');
+
+  const albumElement = document.createElement('div');
+  const albumSpan = document.createElement('span');
+  const groupElement = document.createElement('div');
+  const groupSpan = document.createElement('span');
+  const listenerRankElement = document.createElement('div');
+  const listenerRankSpan = document.createElement('span');
+
+  albumElement.classList.add('item-long');
+  groupElement.classList.add('item');
+  listenerRankElement.classList.add('item-long');
+
+  albumElement.innerHTML = "Debut Album " 
+  albumSpan.innerHTML = guess.debutAlbumYear;
+  groupElement.innerHTML = "Group Size";
+  groupSpan.innerHTML = "1";
+  listenerRankElement.innerHTML = "Listener Rank ";
+  listenerRankSpan.innerHTML = guess.listenerRank;
+
+  albumElement.append(albumSpan);
+  groupElement.append(groupSpan);
+  listenerRankElement.append(listenerRankSpan);
+
+  row2.append(albumElement);
+  row2.append(groupElement);
+  row2.append(listenerRankElement);
+
+  //row3
+  const row3 = document.createElement('div');
+  row3.classList.add('guessRow');
+
+  const genderElement = document.createElement('div');
+  const genderSpan = document.createElement('span');
+  const genreElement = document.createElement('div');
+  const genreSpan = document.createElement('span');
+  const nationalityElement = document.createElement('div');
+  const nationalitySpan = document.createElement('span');
+
+  genderElement.classList.add('item');
+  genreElement.classList.add('item-long');
+  nationalityElement.classList.add('item');
+
+  genderElement.innerHTML = "Gender ";
+  genderSpan.innerHTML = guess.gender;
+  genreElement.innerHTML = "Genre ";
+  genreSpan.innerHTML = guess.genre;
+
+  genderElement.append(genderSpan);
+  genreElement.append(genreSpan);
+
+  const imageNationality = document.createElement('img');
+  imageNationality.src = 'resources\\nationalities\\' + guess.nationality + '.png';
+  nationalityElement.innerHTML = "Nationality ";
+  nationalityElement.append(imageNationality);
+
+  row3.append(genderElement);
+  row3.append(genreElement);
+  row3.append(nationalityElement);
+
+  guessElement.append(row1);
+  guessElement.append(row2);
+  guessElement.append(row3);
+
+  guessContainer.prepend(guessElement);
 }
 
 guessButton.addEventListener('click', handleGuess)
