a2356ef59c62d8a46d1eb04fd7f75b6203fce614
Tommy Rennolds <thomasrennolds@gmail.com>
2024-05-21 11:41:04 -0400

    ad space

--- src/routes/Help.svelte
+++ src/routes/Help.svelte
@@ -183,7 +183,8 @@
             <h2 class="attribute-header">Genre</h2>
             <p class="attribute-text">
               The artists genre. The options are: Rock, Pop, Hip Hop, Country,
-              Alternative, Folk, R&B, Latin, Classical, Jazz, Metal, Electronic, Reggae, and African.
+              Alternative, Folk, R&B, Latin, Classical, Jazz, Metal, Electronic,
+              Reggae, and African.
             </p>
           </div>
           <div class="attribute-blocks">
@@ -206,6 +207,8 @@
           </div>
         </div>
       </div>
+      <div class="ad-space"></div>
+      <div class="ad-space"></div>
     {/if}
   </div>
 </div>
