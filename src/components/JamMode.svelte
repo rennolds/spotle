658cb8a73c2fa494eff6276f39dd2f564c0ef98a
Tommy Rennolds <thomasrennolds@gmail.com>
2025-03-17 21:41:45 -0400

    jam

--- src/components/JamMode.svelte
+++ src/components/JamMode.svelte
@@ -596,7 +596,7 @@
     width: 100%;
     max-width: 340px;
     margin: 0 auto;
-    padding-bottom: 200px; /* Add space at the bottom for better scrolling */
+    padding-bottom: 400px; /* Add space at the bottom for better scrolling */
   }
   
   .intro-content {
