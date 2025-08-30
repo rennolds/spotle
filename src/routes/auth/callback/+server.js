3f8f83710ba7a57f3edac9f53094bc70ee8b7aaa
Tommy Rennolds <thomasrennolds@gmail.com>
2025-08-29 22:08:05 -0400

    TOKEN HASH IS WORKING!

--- src/routes/auth/callback/+server.js
+++ src/routes/auth/callback/+server.js
@@ -5,21 +5,25 @@ export const GET = async (event) => {
 		url,
 		locals: { supabase }
 	} = event;
-	const code = url.searchParams.get('code');
-	const next = url.searchParams.get('next') ?? '/';
-  if (code) {
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) {
-      throw redirect(303, `/${next.slice(1)}`);
-    }
-  }
 
-    const { error } = await supabase.auth.verifyOtp({
-        token_hash: 'hash',
-        type: 'email',
-      });
-    console.error(error);
-  // return the user to an error page with instructions
-  throw redirect(303, '/auth/auth-code-error');
+	const code = url.searchParams.get('code');
+	const tokenHash = url.searchParams.get('token_hash');
+	const type = url.searchParams.get('type');
+	const next = url.searchParams.get('next') ?? '/';
+
+	let error = null;
+
+	if (code) {
+		({ error } = await supabase.auth.exchangeCodeForSession(code));
+	} else if (tokenHash && type) {
+		({ error } = await supabase.auth.verifyOtp({ token_hash: tokenHash, type }));
+	}
+
+	if (!error) {
+		throw redirect(303, `/${next.slice(1)}`);
+	}
+
+	console.error('Auth callback error:', error);
+	throw redirect(303, '/auth/auth-code-error');
 };
 
