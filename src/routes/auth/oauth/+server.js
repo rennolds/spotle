8d3471881e96c8069503680e141e2fc9b3314ff2
Tommy Rennolds <thomasrennolds@gmail.com>
2025-08-31 16:43:47 -0400

    improving auth, a month of artists

--- src/routes/auth/oauth/+server.js
+++ src/routes/auth/oauth/+server.js
@@ -1,3 +1,5 @@
+import { redirect } from '@sveltejs/kit';
+
 export const GET = async (event) => {
   const { url, locals: { supabase } } = event;
 
@@ -12,16 +14,10 @@ export const GET = async (event) => {
   });
 
   if (error || !data?.url) {
-    return new Response(null, {
-      status: 303,
-      headers: new Headers({ Location: '/auth/auth-code-error' })
-    });
+    throw redirect(303, '/auth/auth-code-error');
   }
 
-  return new Response(null, {
-    status: 303,
-    headers: new Headers({ Location: data.url })
-  });
+  throw redirect(303, data.url);
 };
 
 
