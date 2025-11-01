40576b762f4ab31b45165750f5efe90e6801f53f
Tommy Rennolds <thomasrennolds@gmail.com>
2025-11-01 10:42:03 -0400

    monitor endpoint

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,39 +3,39 @@ import { sequence } from '@sveltejs/kit/hooks';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 const supabase = async ({ event, resolve }) => {
-	event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-		cookies: {
-			getAll: () => event.cookies.getAll(),
-			setAll: (cookiesToSet) => {
-				cookiesToSet.forEach(({ name, value, options }) => {
-					event.cookies.set(name, value, { ...options, path: '/' });
-				});
-			}
-		}
-	});
+	// event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+	// 	cookies: {
+	// 		getAll: () => event.cookies.getAll(),
+	// 		setAll: (cookiesToSet) => {
+	// 			cookiesToSet.forEach(({ name, value, options }) => {
+	// 				event.cookies.set(name, value, { ...options, path: '/' });
+	// 			});
+	// 		}
+	// 	}
+	// });
 
-	event.locals.safeGetSession = async () => {
-		const {
-			data: { session }
-		} = await event.locals.supabase.auth.getSession();
-		if (!session) {
-			return { session: null, user: null };
-		}
+	// event.locals.safeGetSession = async () => {
+	// 	const {
+	// 		data: { session }
+	// 	} = await event.locals.supabase.auth.getSession();
+	// 	if (!session) {
+	// 		return { session: null, user: null };
+	// 	}
 
-		const {
-			data: { user },
-			error
-		} = await event.locals.supabase.auth.getUser();
-		if (error) {
-			// JWT validation has failed
-			return { session: null, user: null };
-		}
+	// 	const {
+	// 		data: { user },
+	// 		error
+	// 	} = await event.locals.supabase.auth.getUser();
+	// 	if (error) {
+	// 		// JWT validation has failed
+	// 		return { session: null, user: null };
+	// 	}
 
-		return { session, user };
-	};
+	// 	return { session, user };
+	// };
 
-	// Ensure any auth cookie writes happen before the response is generated
-	await event.locals.supabase.auth.getSession();
+	// // Ensure any auth cookie writes happen before the response is generated
+	// await event.locals.supabase.auth.getSession();
 
 	return resolve(event, {
 		filterSerializedResponseHeaders(name) {
@@ -48,4 +48,4 @@ const supabase = async ({ event, resolve }) => {
 	});
 };
 
-export const handle = sequence(supabase);
+export const handle = ({ resolve, event }) => resolve(event);
